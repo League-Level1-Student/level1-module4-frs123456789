706ccc873ce464ba1df89c3fb0fe88711c46df72
frs123456789 <league@WTS14-2.attlocal.net>
2019-01-16 19:54:50 -0800

    Simon Says Complete

--- src/SimonSays.java
+++ src/SimonSays.java
@@ -42,33 +42,47 @@ JOptionPane.showMessageDialog(null, "Press the matching key when Simon says, oth
 		// 4. Call the showImage method to show an image
 showImage();
 	}
-
+	int points = 0;
 	public void keyPressed(KeyEvent e) {
 		// 15. Make a points variable to track the score.
-
-		// 16. If the keyCode matches the imageIndex and "Simon says"
 		
+		
+		
+		// 16. If the keyCode matches the imageIndex and "Simon says"
+		if(e.getKeyCode() == imageIndex && simonSays == true) {
+			 points = points + 1;
+			speak("you are correct!");
+		}
 			// 17. Increase the value of score
 		
 			// 18. Use the speak method to tell the user they were correct
 		
 		// 19. If the keyCode doesn't match the imageIndex and "Simon didn't say..."
-		
+		else if(e.getKeyCode() != imageIndex && simonSays == false){
+			points = points + 1;
+			speak("you are correct!");
+		}
 			// 20.  Increase the value of score
 		
 			// 21. Use the speak method to tell the user they were correct
 		
 		// 22. Increment tries by 1
-		
+		tries = tries + 1;
 		// 25. If tries is greater than 9 (or however many you want)...
-		
+		if(tries > 5) {
+			speak("Your score is " + points);
+			System.exit(0);
+			
+			
+		}
 			// 26. Tell the user their score
 		
 			// 27. Exit the program
 
 		// 23. Dispose of the frame
-
+		jf.dispose();
 		// 24. Call the showImage method to show a new image
+		showImage();
 	}
 
 	private void showImage() {
@@ -84,10 +98,21 @@ jf.setName("framey");
 		// 9. Pack the frame
 		jf.pack();
 		// 10. Set the defaultCloseOperation of your from to JFrame.EXIT_ON_CLOSE
-		jf.EXIT_ON_CLOSE ;
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 11. Add a key listener to the frame
-
+jf.addKeyListener(this);
 		// 12. Create a new instance of Random
+Random ran = new Random();
+int num = ran.nextInt(2);
+if(num == 0) {
+	speak("Simon says press THIS key.");
+	simonSays = true;
+}
+else if(num == 1) {
+	
+	speak("press THIS key.");
+	simonSays = false;
+}
 
 		// 13. Use the Random and the speak method to either say 
 		// "Simon says press this key" or "Press this key"
